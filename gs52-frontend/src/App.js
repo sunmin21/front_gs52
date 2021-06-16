5739284f17d72fdb743ccca3bd05bc197539734e
jiyun0524 <jiyun9705@gmail.com>
2021-06-16 20:32:28 +0900

    jy_0616
    공휴일 페이지 1차

--- gs52-frontend/src/App.js
+++ gs52-frontend/src/App.js
@@ -1,9 +1,24 @@
 import logo from "./logo.svg";
 import "./App.css";
-import test from "./pages/test";
-import { Route } from "react-router";
+import React, { Component } from "react";
+import { HashRouter, Route, Switch } from "react-router-dom";
+import "./scss/style.scss";
+const loading = (
+  <div className="pt-3 text-center">
+    <div className="sk-spinner sk-spinner-pulse"></div>
+  </div>
+);
+const Test = React.lazy(() => import("./pages/test"));
 function App() {
-  return <Route component={test} path="/" />;
+  return (
+    <HashRouter>
+      <React.Suspense fallback={loading}>
+        <Switch>
+          <Route render={(props) => <Test {...props} />} path="/" />;
+        </Switch>
+      </React.Suspense>
+    </HashRouter>
+  );
 }
 
 export default App;
