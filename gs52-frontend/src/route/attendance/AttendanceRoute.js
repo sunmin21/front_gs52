1a93df74c048a9b93a62e2a0d93e2954af30d678
wnstn819 <48230951+wnstn819@users.noreply.github.com>
2021-06-17 11:30:38 +0900

    근태관리-연차관리 연결
    근태관리-연차관리 연결

--- gs52-frontend/src/route/attendance/AttendanceRoute.js
+++ gs52-frontend/src/route/attendance/AttendanceRoute.js
@@ -1,9 +1,16 @@
 import React from "react";
 const Dashboard = React.lazy(() => import("../../views/dashboard/Dashboard"));
+const AnnualManage = React.lazy(() =>
+  import("../../containers/attendance/AnnualManagement")
+);
 
 const AttendanceRoute = [
   { path: "/attendance/schedule", name: "구성원스케줄", component: Dashboard },
-  { path: "/attendance/AnnualManage", name: "연차관리", component: Dashboard },
+  {
+    path: "/attendance/AnnualManagement",
+    name: "연차관리",
+    component: AnnualManage,
+  },
   { path: "/attendance/insite", name: "인사이트", component: Dashboard },
   { path: "/attendance", name: "근태관리", component: Dashboard },
 ];
