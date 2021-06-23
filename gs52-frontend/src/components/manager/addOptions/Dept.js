77172729da2a4607d32257dc77ba6dd27eb2319e
wpdbsx <wpdbsx@naver.com>
2021-06-23 19:35:55 +0900

    관리자페이지- 항목추가

--- gs52-frontend/src/components/manager/addOptions/Dept.js
+++ gs52-frontend/src/components/manager/addOptions/Dept.js
@@ -1,4 +1,11 @@
-import { CButton, CCardBody, CCollapse, CDataTable } from "@coreui/react";
+import {
+  CAlert,
+  CButton,
+  CCardBody,
+  CCollapse,
+  CDataTable,
+  CPopover,
+} from "@coreui/react";
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useHistory, useLocation } from "react-router-dom";
@@ -11,7 +18,7 @@ const Dept = () => {
   let { dept } = useSelector(({ manager }) => ({
     dept: manager.dept,
   }));
-
+  const [show, setShow] = useState(false);
   useEffect(() => {
     dispatch(deptAxios());
   }, [dispatch]);
@@ -23,6 +30,7 @@ const Dept = () => {
   const deptData = dept.map((item) => ({
     인덱스: item.dept_INDEX,
     부서이름: item.dept_NAME,
+    팀COUNT: item.team_COUNT,
   }));
   const toggleDetails = (index) => {
     const position = details.indexOf(index);
@@ -38,6 +46,7 @@ const Dept = () => {
   const fields = [
     { key: "인덱스", _style: { width: "20%" } },
     { key: "부서이름", _style: { width: "40%" } },
+    { key: "팀COUNT", _style: { width: "40%" } },
 
     {
       key: "show_details",
@@ -82,16 +91,31 @@ const Dept = () => {
           return (
             <CCollapse show={details.includes(index)}>
               <CCardBody>
+                <CAlert
+                  color="danger"
+                  show={show}
+                  closeButton
+                  onClick={() => {
+                    setShow(false);
+                  }}
+                >
+                  부서 안에 팀이 존재합니다.
+                </CAlert>
                 <CButton size="sm" color="info" onClick={() => {}}>
                   부서수정
                 </CButton>
+
                 <CButton
                   size="sm"
                   color="danger"
                   className="ml-1"
                   onClick={() => {
-                    DeleteDept(item.인덱스);
-                    dispatch(deptAxios());
+                    if (item.팀COUNT === 0) {
+                      DeleteDept(item.인덱스);
+                      dispatch(deptAxios());
+                    } else {
+                      setShow(true);
+                    }
                   }}
                 >
                   부서삭제
