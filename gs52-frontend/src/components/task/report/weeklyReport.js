1efe1ee7ca042939719f8268678fbfb694ca4b54
jiyun0524 <jiyun9705@gmail.com>
2021-06-24 18:37:50 +0900

    0624

--- gs52-frontend/src/components/task/report/weeklyReport.js
+++ gs52-frontend/src/components/task/report/weeklyReport.js
@@ -1,8 +1,26 @@
-import React from "react";
+import React, {useState} from "react";
+import DatePicker from 'react-datepicker'
+import 'react-datepicker/dist/react-datepicker.css';
+import { CCard, CCardBody, CCardHeader } from '@coreui/react';
 
 function WeeklyReport() {
+    const [startDate, setStartDate] = useState(new Date());
+
     return (
-        <h1>먀아앙</h1>
+        <CCard>
+            <CCardHeader>
+                주간 보고서
+            </CCardHeader>
+            <CCardBody>
+                <DatePicker
+                    selected={startDate}
+                    onChange={(date) => setStartDate(date)}
+                    // inline // 달력이 모달창에 뜨도록
+                    // minDate={new Date()} // 이전 날은 선택 못하도록
+                    popperPlacement="auto" // 화면 중앙에 오도록
+                />
+            </CCardBody>
+        </CCard >
     )
 }
 
