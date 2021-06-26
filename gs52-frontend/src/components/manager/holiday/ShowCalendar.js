8682e5ebde2657f3f52c8c2ae30fc36ef65d3a01
wnstn819 <48230951+wnstn819@users.noreply.github.com>
2021-06-26 15:05:32 +0900

    스케쥴러 디스패치
    스케쥴러 디스패치 삭제

--- gs52-frontend/src/components/manager/holiday/ShowCalendar.js
+++ gs52-frontend/src/components/manager/holiday/ShowCalendar.js
@@ -6,7 +6,7 @@ import { DeleteHoliday } from "src/lib/api/manager/holiday/HolidayAPI";
 import { useDispatch, useSelector } from "react-redux";
 import { holidayAxios } from "src/modules/manager/holiday";
 
-import holidaydata from './HolidayData';
+import holidaydata from "./HolidayData";
 
 function ShowCalendar() {
   const [events, setEvents] = useState([]);
@@ -17,74 +17,59 @@ function ShowCalendar() {
     };
   });
 
-    const dispatch = useDispatch();
-    const { holiday } = useSelector((state) => {
-        // console.log("나 !!!!!!!!!!!!!!!!!")
-        // console.log(state)
-        return ({
-            holiday : state.holiday.holiday
-        })
-    });
+  useEffect(() => {
+    dispatch(holidayAxios());
+  }, [dispatch]);
 
-    useEffect(() => {
-        dispatch(holidayAxios())
-    }, [dispatch])
+  const eventOnclick = (e) => {
+    var msg = "삭제하시겠습니까?";
 
-    const eventOnclick = (e) => {
-        var msg = ("삭제하시겠습니까?");
-
-        if(e.event._def["publicId"] > 0) {
-            if (window.confirm(msg) != 0)
-            {
-                console.log("삭제")
-                console.log(e.event._def)
-                // holiday_index를 가져옴
-                DeleteHoliday(e.event._def["publicId"]);
-                dispatch(holidayAxios())
-                // 자동 rendering
-            }
-            else {
-                console.log("삭제취소")
-            }
-        }
-        else {
-            alert("국가 공휴일은 삭제할 수 없습니다.")
-        }
-
+    if (e.event._def["publicId"] > 0) {
+      if (window.confirm(msg) != 0) {
+        console.log("삭제");
+        console.log(e.event._def);
+        // holiday_index를 가져옴
+        DeleteHoliday(e.event._def["publicId"]);
+        dispatch(holidayAxios());
+        // 자동 rendering
+      } else {
+        console.log("삭제취소");
+      }
+    } else {
+      alert("국가 공휴일은 삭제할 수 없습니다.");
     }
-    // console.log(holiday)
-    const data = holiday.map((item) => {
-        return ({
-            id: item.holiday_INDEX,
-            title: item.holiday_TITLE,
-            start: item.holiday_DATE,
-            annual : item.holiday_ANNUAL_REPEAT
-        })
-    })
+  };
+  // console.log(holiday)
+  const data = holiday.map((item) => {
+    return {
+      id: item.holiday_INDEX,
+      title: item.holiday_TITLE,
+      start: item.holiday_DATE,
+      annual: item.holiday_ANNUAL_REPEAT,
+    };
+  });
 
-    return (
-        <CCard>
-            <CCardHeader>
-                휴일 설정
-            </CCardHeader>
-            <CCardBody>
-                <div className="calendarBox">
-                <FullCalendar
-                    // height="600px"
-                    contentHeight="500px"
-                    defaultView="dayGridMonth"
-                    plugins={[daygridPlugin]}
-                    eventSources = {[data, holidaydata]}
-                    // events={data}
-                    // events={holidaydata}
-                    eventClick={eventOnclick}
-                    eventColor="white"
-                    eventTextColor="red"
-                    eventDisplay="title"
-                    />
-                </div>
-            </CCardBody>
-        </CCard >
-    )
+  return (
+    <CCard>
+      <CCardHeader>휴일 설정</CCardHeader>
+      <CCardBody>
+        <div className="calendarBox">
+          <FullCalendar
+            // height="600px"
+            contentHeight="500px"
+            defaultView="dayGridMonth"
+            plugins={[daygridPlugin]}
+            eventSources={[data, holidaydata]}
+            // events={data}
+            // events={holidaydata}
+            eventClick={eventOnclick}
+            eventColor="white"
+            eventTextColor="red"
+            eventDisplay="title"
+          />
+        </div>
+      </CCardBody>
+    </CCard>
+  );
 }
 export default ShowCalendar;
