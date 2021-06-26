731a2a6e065e1359d88837db63687596d2fb1b19
wnstn819 <48230951+wnstn819@users.noreply.github.com>
2021-06-26 15:10:08 +0900

    테스트
    테스트

--- gs52-frontend/src/modules/index.js
+++ gs52-frontend/src/modules/index.js
@@ -4,10 +4,11 @@ import test from "./test";
 import manager, { managerSaga } from "./manager/manager";
 import emp from "./emp/emp";
 import task, { taskSaga } from "./task/task";
-import conf_check,{ConfSaga2} from './schedule/conf'
-import holiday, { holidaySaga2 } from './manager/holiday';
+import conf_check, { ConfSaga2 } from "./schedule/conf";
+import holiday, { holidaySaga2 } from "./manager/holiday";
 import report, { reportSaga2 } from "./task/report";
 import changeState, { changeStore } from "./changeStore";
+import annual, { annualSaga2 } from "./annual/annual";
 
 const rootReducer = combineReducers({
   test,
@@ -16,10 +17,17 @@ const rootReducer = combineReducers({
   holiday,
   emp,
   task,
-  conf_check
+  conf_check,
+  annual,
 });
 
 export function* rootSaga() {
-  yield all([taskSaga(),holidaySaga2(),ConfSaga2(),reportSaga2()]);
+  yield all([
+    taskSaga(),
+    holidaySaga2(),
+    ConfSaga2(),
+    reportSaga2(),
+    annualSaga2(),
+  ]);
 }
 export default rootReducer;
