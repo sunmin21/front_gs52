f5396e46379037f9f88e59a0874eb75719a18fef
jiyun0524 <jiyun9705@gmail.com>
2021-06-28 17:40:24 +0900

    0628
    insert delete

--- gs52-frontend/src/components/task/report/weeklyReport.js
+++ gs52-frontend/src/components/task/report/weeklyReport.js
@@ -5,6 +5,7 @@ import { CCard, CCardBody, CCardHeader, CCardGroup, CDataTable } from '@coreui/r
 import { useDispatch, useSelector } from 'react-redux';
 import { reportAxios, delreportAxios } from 'src/modules/task/report';
 import AddReport from "./AddReport";
+import { DeleteReport } from "src/lib/api/task/ReportAPI";
 
 function WeeklyReport() {
 
@@ -59,14 +60,18 @@ function WeeklyReport() {
         return (dispatch(reportAxios({ emp, weekStart: moment(weekStart).day(0).format("YYYY-MM-DD"), weekEnd: moment(weekStart).day(6).format("YYYY-MM-DD") })))
     }
 
+    // 열 클릭시 삭제 기능
     const eventOnclick = (e) => {
+        console.log(e.id)
         var msg = "삭제하시겠습니까?";
         
         if (window.confirm(msg) != 0) {
             console.log("삭제");
             // report_index를 가져옴
-            dispatch(delreportAxios({id : e.id}));
+            // dispatch(delreportAxios({id : e.id}));
             // 자동 rendering
+            DeleteReport(e.id);
+            dispatch(reportAxios())
             // 자동 렌더링이 안돼 @@@@@@@@@@@@@@@@@@@@@@@@
             
         } else {
