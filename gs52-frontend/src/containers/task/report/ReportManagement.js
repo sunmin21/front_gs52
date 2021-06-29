e2f7d7b2e1df6633abe5932eb2c97a7400731824
wpdbsx <wpdbsx@naver.com>
2021-06-29 20:05:41 +0900

    대문자

--- gs52-frontend/src/containers/task/report/ReportManagement.js
+++ gs52-frontend/src/containers/task/report/ReportManagement.js
@@ -1,22 +1,21 @@
-import React from 'react'
-import { CRow, CCol, CCardGroup, CCardBody  } from '@coreui/react'
-import CheckOthers from 'src/components/task/report/CheckOthers'
-import WeeklyReport from 'src/components/task/report/WeeklyReport'
+import React from "react";
+import { CRow, CCol, CCardGroup, CCardBody } from "@coreui/react";
+import CheckOthers from "src/components/task/report/CheckOthers";
+import WeeklyReport from "src/components/task/report/WeeklyReport";
 
 const HolidayManagement = () => {
-    
-    return (
-        <CRow>
-            <CCol>
-                <CCardBody>
-                    <CheckOthers/>
-                </CCardBody>
-                <CCardBody>
-                    <WeeklyReport />
-                </CCardBody>
-            </CCol>
-        </CRow>
-    )
-}
+  return (
+    <CRow>
+      <CCol>
+        <CCardBody>
+          <CheckOthers />
+        </CCardBody>
+        <CCardBody>
+          <WeeklyReport />
+        </CCardBody>
+      </CCol>
+    </CRow>
+  );
+};
 
-export default HolidayManagement
+export default HolidayManagement;
