ca1b7cfbba6591cddd8dfdc9b4257fa4a1eead73
jiyun0524 <jiyun9705@gmail.com>
2021-07-02 14:53:47 +0900

    0702

--- gs52-frontend/src/components/shedule/project/ProjectList.js
+++ gs52-frontend/src/components/shedule/project/ProjectList.js
@@ -1,12 +1,11 @@
 import React, { useState } from "react";
 import { CCardBody, CDataTable, CBadge, CButton, CCollapse, CCard, CTabPane } from "@coreui/react";
 const usersData = [
-    {id: 0, name: 'John Doe', registered: '2018/01/01', role: 'Guest', status: 'Pending'},
-    {id: 1, name: 'Samppa Nori', registered: '2018/01/01', role: 'Member', status: 'Active'},
-    {id: 2, name: 'Estavan Lykos', registered: '2018/02/01', role: 'Staff', status: 'Banned'},
-    {id: 3, name: 'Chetan Mohamed', registered: '2018/02/01', role: 'Admin', status: 'Inactive'},
-    {id: 4, name: 'Derick Maximinus', registered: '2018/03/01', role: 'Member', status: 'Pending'},
-    {id: 5, name: 'Friderik Dávid', registered: '2018/01/21', role: 'Staff', status: 'Active'}
+    {번호: 1, 프로젝트명: '첫 번째 프로젝트 어쩌구 저쩌구', 시작: '2021/03/01', 종료: '2021/04/01', 담당자: 'Guest'},
+    {번호: 2, 프로젝트명: '이게 바로 두 번째 프로젝트다', 시작: '2021/04/01', 종료: '2021/04/01', 담당자: 'Member'},
+    {번호: 3, 프로젝트명: '리액트를 이용한 세 번째 프로젝트', 시작: '2021/05/01', 종료: '2021/04/01', 담당자: 'Staff'},
+    {번호: 4, 프로젝트명: '네 번째 프로젝트는 스프링 부트', 시작: '2021/06/01', 종료: '2021/04/01', 담당자: 'Admin'},
+    {번호: 5, 프로젝트명: '가장 최근에 생긴 다섯 번째 프로젝트', 시작: '2021/07/01', 종료: '2021/04/01', 담당자: 'Member'},
 ]
 
 function ProjectList() {
@@ -26,28 +25,18 @@ function ProjectList() {
 
 
     const fields = [
-        { key: 'name', _style: { width: '40%'} },
-        'registered',
-        { key: 'role', _style: { width: '20%'} },
-        { key: 'status', _style: { width: '20%'} },
+        { key: '번호', _style: { width: '5%'} },
+        { key: '프로젝트명', _style: { width: '50%'} },
+        '시작', '종료',
+        { key: '담당자', _style: { width: '20%'} },
         {
         key: 'show_details',
         label: '',
-        _style: { width: '1%' },
+        _style: { width: '7%' },
         sorter: false,
         filter: false
         }
     ]
-
-    const getBadge = (status)=>{
-        switch (status) {
-        case 'Active': return 'success'
-        case 'Inactive': return 'secondary'
-        case 'Pending': return 'warning'
-        case 'Banned': return 'danger'
-        default: return 'primary'
-        }
-    }
 
     return (
         <CCardBody>
@@ -60,17 +49,9 @@ function ProjectList() {
                 itemsPerPageSelect
                 itemsPerPage={5}
                 hover
-                sorter
+                sorterValue={{ column: "번호", desc: "false" }}
                 pagination
                 scopedSlots = {{
-                    'status':
-                    (item)=>(
-                        <td>
-                        <CBadge color={getBadge(item.status)}>
-                            {item.status}
-                        </CBadge>
-                        </td>
-                    ),
                     'show_details':
                     (item, index)=>{
                         return (
@@ -82,7 +63,7 @@ function ProjectList() {
                             size="sm"
                             onClick={()=>{toggleDetails(index)}}
                             >
-                            {details.includes(index) ? 'Hide' : 'Show'}
+                            {details.includes(index) ? '간단히' : '자세히'}
                             </CButton>
                         </td>
                         )
