938062361d0f50f4319c68fc03df38cc4568a42d
jiyun0524 <jiyun9705@gmail.com>
2021-07-03 17:18:24 +0900

    0703

--- gs52-frontend/src/components/shedule/project/ProjectList.js
+++ gs52-frontend/src/components/shedule/project/ProjectList.js
@@ -4,7 +4,43 @@ import { useHistory } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { proceedingAxios } from "src/modules/schedule/project/projectList";
 import { projectNoChange } from "src/modules/schedule/project/project";
-
+const usersData = [
+    {
+        번호: 1,
+        프로젝트명: "첫 번째 프로젝트 어쩌구 저쩌구",
+        시작: "2021-03-01",
+        종료: "2021-04-01",
+        담당자: "Guest",
+    },
+    {
+        번호: 2,
+        프로젝트명: "이게 바로 두 번째 프로젝트다",
+        시작: "2021-04-01",
+        종료: "2021-05-01",
+        담당자: "Member",
+    },
+    {
+        번호: 3,
+        프로젝트명: "리액트를 이용한 세 번째 프로젝트",
+        시작: "2021-05-01",
+        종료: "2021-06-01",
+        담당자: "Staff",
+    },
+    {
+        번호: 4,
+        프로젝트명: "네 번째 프로젝트는 스프링 부트",
+        시작: "2021-06-01",
+        종료: "2021-07-01",
+        담당자: "Admin",
+    },
+    {
+        번호: 5,
+        프로젝트명: "가장 최근에 생긴 다섯 번째 프로젝트",
+        시작: "2021-07-01",
+        종료: "2021-08-01",
+        담당자: "Member",
+    },
+    ];
 function ProjectList() {
     let [emp] = useState(8);
     const history = useHistory();
@@ -41,7 +77,7 @@ function ProjectList() {
     return (
         <CCardBody>
             <CDataTable
-                items={data}
+                items={usersData}
                 fields={fields}
                 columnFilter
                 tableFilter
