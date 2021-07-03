7fa1a9be288da07f8c83ac1cb4662d73c2327e20
wpdbsx <wpdbsx@naver.com>
2021-07-03 16:44:24 +0900

    useHistory

--- gs52-frontend/src/components/shedule/project/ProjectList.js
+++ gs52-frontend/src/components/shedule/project/ProjectList.js
@@ -6,61 +6,61 @@ import { proceedingAxios } from "src/modules/schedule/project/projectList";
 import { projectNoChange } from "src/modules/schedule/project/project";
 
 function ProjectList() {
-    let [emp] = useState(8);
+  let [emp] = useState(8);
+  const history = useHistory();
+  const dispatch = useDispatch();
 
-    const dispatch = useDispatch();
+  const { proceeding } = useSelector((state) => {
+    console.log(state);
+    return {
+      proceeding: state.project.proceeding,
+    };
+  });
 
-    const { proceeding } = useSelector((state) => {
-        console.log(state);
-        return {
-        proceeding: state.project.proceeding,
-        };
-    });
+  useEffect(() => {
+    dispatch(proceedingAxios({ emp }));
+  }, [dispatch]);
 
-    useEffect(() => {
-        dispatch(proceedingAxios({ emp }));
-    }, [dispatch]);
+  const fields = [
+    { key: "번호", _style: { width: "5%" } },
+    { key: "프로젝트명", _style: { width: "50%" } },
+    "시작",
+    "종료",
+    // { key: "담당자", _style: { width: "20%" } },
+  ];
 
-    const fields = [
-        { key: "번호", _style: { width: "5%" } },
-        { key: "프로젝트명", _style: { width: "50%" } },
-        "시작",
-        "종료",
-        // { key: "담당자", _style: { width: "20%" } },
-    ];
+  const data = proceeding.map((item) => {
+    return {
+      번호: item.project_INDEX,
+      프로젝트명: item.project_TITLE,
+      시작: item.project_START,
+      종료: item.project_END,
+    };
+  });
 
-    const data = proceeding.map((item) => {
-        return {
-        번호: item.project_INDEX,
-        프로젝트명: item.project_TITLE,
-        시작: item.project_START,
-        종료: item.project_END,
-        };
-    });
-
-    return (
-        <CCardBody>
-        <CDataTable
-            items={data}
-            fields={fields}
-            columnFilter
-            tableFilter
-            footer
-            itemsPerPageSelect
-            itemsPerPage={5}
-            hover
-            sorter
-            sorterValue={{ column: "번호", desc: "true" }}
-            pagination
-            onRowClick={(item) => {
-                history.push({
-                    pathname: `/schedule/project/detail`,
-                });
-            dispatch(projectNoChange({ index: item.번호 }));
-            }}
-        />
-        </CCardBody>
-    );
+  return (
+    <CCardBody>
+      <CDataTable
+        items={data}
+        fields={fields}
+        columnFilter
+        tableFilter
+        footer
+        itemsPerPageSelect
+        itemsPerPage={5}
+        hover
+        sorter
+        sorterValue={{ column: "번호", desc: "true" }}
+        pagination
+        onRowClick={(item) => {
+          history.push({
+            pathname: `/schedule/project/detail`,
+          });
+          dispatch(projectNoChange({ index: item.번호 }));
+        }}
+      />
+    </CCardBody>
+  );
 }
 
 export default ProjectList;
