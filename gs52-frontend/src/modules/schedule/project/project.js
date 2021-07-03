94ca96b4b07c5368cf910d564e495af2cabe58b8
jiyun0524 <jiyun9705@gmail.com>
2021-07-03 16:12:30 +0900

    0703

--- gs52-frontend/src/modules/schedule/project/project.js
+++ gs52-frontend/src/modules/schedule/project/project.js
@@ -2,40 +2,58 @@ import { createAction, handleActions } from "redux-actions";
 import createRequestSaga, {
   createRequestActionTypes,
 } from "../../../lib/createRequestSaga";
-import * as API from "../../../lib/api/task/ReportAPI";
+import * as API from "../../../lib/api/schedule/Project";
 import { takeLatest } from "redux-saga/effects";
 
-const [PROJECT, PROJECT_SUCCESS, PROJECT_FAILURE] =
-  createRequestActionTypes("schedule/PROJECT"); //타입유형
+// const [PROJECT, PROJECT_SUCCESS, PROJECT_FAILURE] =
+  // createRequestActionTypes("schedule/PROJECT"); //타입유형
+const [PROCEEDING, PROCEEDING_SUCCESS, PROCEEDING_FAILURE] =
+  createRequestActionTypes("schedule/PROCEEDING"); //타입유형
 
-export const projectAxios = createAction(PROJECT, ({ 인덱스 }) => ({
-  인덱스,
-})); //리덕스의 액션함수
+// export const projectAxios = createAction(PROJECT, ({ 인덱스 }) => ({
+//   인덱스,
+// })); //리덕스의 액션함수
+export const proceedingAxios = createAction(PROCEEDING); //리덕스의 액션함수
 
-const projectSelectSaga = createRequestSaga(PROJECT, API.SelectReport);
+// const projectSelectSaga = createRequestSaga(PROJECT, API.SelectProject);
+const proceedingSaga = createRequestSaga(PROCEEDING, API.SelectProceeding);
 
 export function* projectSaga2() {
-  yield takeLatest(PROJECT, projectSelectSaga);
+  // yield takeLatest(PROJECT, projectSelectSaga);
+  yield takeLatest(PROCEEDING, proceedingSaga);
 }
 
 const initialState = {
   //초기값을 정의
-  project: {},
-  projectError: null,
+  // project: {},
+  // projectError: null,
+  proceeding: [],
+  proceedingError: null,
 };
 
 // 리듀서 선언부분
 const project = handleActions(
   {
-    [PROJECT_SUCCESS]: (state, { payload: project }) => ({
+    // [PROJECT_SUCCESS]: (state, { payload: project }) => ({
+    //   ...state,
+    //   projectError: null,
+    //   project,
+    // }),
+
+    // [PROJECT_FAILURE]: (state, { payload: error }) => ({
+    //   ...state,
+    //   projectError: error,
+    // }),
+
+    [PROCEEDING_SUCCESS]: (state, { payload: proceeding }) => ({
       ...state,
-      projectError: null,
-      project,
+      proceedingError: null,
+      proceeding,
     }),
 
-    [PROJECT_FAILURE]: (state, { payload: error }) => ({
+    [PROCEEDING_FAILURE]: (state, { payload: error }) => ({
       ...state,
-      projectError: error,
+      proceedingError: error,
     }),
   },
   initialState
