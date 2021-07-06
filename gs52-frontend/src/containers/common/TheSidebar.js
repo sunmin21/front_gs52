5d7d708f16b597cf3ac7fc439e14f32a812e756d
jiyun0524 <jiyun9705@gmail.com>
2021-07-06 17:40:27 +0900

    0706

--- gs52-frontend/src/containers/common/TheSidebar.js
+++ gs52-frontend/src/containers/common/TheSidebar.js
@@ -24,7 +24,7 @@ const TheSidebar = ({ nav }) => {
 
   return (
     <CSidebar
-      style={{backgroundColor:"white"}}
+      style={{backgroundColor:"#EAEAEA"}}
       show={show}
       onShowChange={(val) => dispatch({ type: "set", sidebarShow: val })}
     >
