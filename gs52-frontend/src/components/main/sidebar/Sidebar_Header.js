c1db65f5eab5a178556af75ab20b319f771db065
jiyun0524 <jiyun9705@gmail.com>
2021-07-06 20:01:27 +0900

    0706

--- gs52-frontend/src/components/main/sidebar/Sidebar_Header.js
+++ gs52-frontend/src/components/main/sidebar/Sidebar_Header.js
@@ -14,9 +14,9 @@ function Sidebar_Header(){
 
     return(
         <div>
-        <button onClick={onWork}>추우우울그으으은</button><br/>
-        
-        {user.id}<br/>
+        <button onClick={onWork}><h2>출근</h2></button><br/>
+        <br />
+        <h2>{user.id}</h2><br/>
         {user.username}님, 접속하였습니다.<br/>
         여기에다가 유저정보넣자아아</div>
     )
