50494291dc9b0ba7f2745ed0e2d2e5c1045c50c3
jiyun0524 <jiyun9705@gmail.com>
2021-07-15 11:06:36 +0900

    0715

--- gs52-frontend/src/components/main/annual/annual.js
+++ gs52-frontend/src/components/main/annual/annual.js
@@ -7,12 +7,15 @@ const annualCard = (props) => {
       <CCardBody>
         <Row>
           {props.empvacation.length !== 0 && props.near.length !== 0 ? (
-            <Col>
-              <h4>
-                {props.empvacation[0].emp_NAME}님의 잔여 휴가 :
-                {props.empvacation[0].emp_VACATION}일
-              </h4>
-              <h4>다음 휴가는 {props.near[0].vacation_DATE}일 입니다</h4>
+            <Col >
+              <div style={{whiteSpace:"nowrap"}}>
+
+              
+              <h5 >{props.empvacation[0].emp_NAME}님의 잔여 휴가</h5>
+              <h1>{props.empvacation[0].emp_VACATION}일</h1>
+              
+              <h4>다음 휴가는 <h1>{props.near[0].vacation_DATE}</h1> 입니다</h4>
+            </div>
             </Col>
           ) : (
             <h4>휴가 정보가 없습니다.</h4>
