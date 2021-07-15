74e3932f1ea26f9465fbbd42d15b8676bf6e9c3a
wnstn819 <48230951+wnstn819@users.noreply.github.com>
2021-07-15 11:06:01 +0900

    휴가 예외처리
    휴가 예외처리

--- gs52-frontend/src/components/main/annual/annual.js
+++ gs52-frontend/src/components/main/annual/annual.js
@@ -1,10 +1,10 @@
-import { CCard, CCardBody } from "@coreui/react";
+import { CCard } from "@coreui/react";
 import { Col, Row } from "antd";
 
 const annualCard = (props) => {
   return (
     <>
-      <CCardBody>
+      <CCard>
         <Row>
           {props.empvacation.length !== 0 && props.near.length !== 0 ? (
             <Col>
@@ -14,11 +14,19 @@ const annualCard = (props) => {
               </h4>
               <h4>다음 휴가는 {props.near[0].vacation_DATE}일 입니다</h4>
             </Col>
+          ) : props.empvacation.length !== 0 && props.near.length == 0 ? (
+            <Col>
+              <h4>
+                {props.empvacation[0].emp_NAME}님의 잔여 휴가 :
+                {props.empvacation[0].emp_VACATION}일
+                <h4> 다음 휴가 정보가 없습니다.</h4>
+              </h4>
+            </Col>
           ) : (
-            <h4>휴가 정보가 없습니다.</h4>
+            <h4> 휴가 정보를 확인해주세요</h4>
           )}
         </Row>
-      </CCardBody>
+      </CCard>
     </>
   );
 };
