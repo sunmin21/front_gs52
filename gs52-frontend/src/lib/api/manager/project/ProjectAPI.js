e45c446f70bd036d26e9f9b81563eb16928c8969
jiyun0524 <jiyun9705@gmail.com>
2021-07-20 18:46:42 +0900

    lsm_project role

--- gs52-frontend/src/lib/api/manager/project/ProjectAPI.js
+++ gs52-frontend/src/lib/api/manager/project/ProjectAPI.js
@@ -1,7 +1,11 @@
 import client from "../../client";
+import { getCurrentUser } from "src/lib/api/jwt/LoginAPI";
 
-const API_URL = "http://192.168.20.17:8081";
+// const API_URL = "http://http://localhost:8081";
+const API_URL = "http://localhost:8081";
 
+const user = getCurrentUser();
+  
 if (localStorage.getItem("accessToken") != null) {
   client.defaults.headers.common.Authorization = `Bearer ${localStorage
     .getItem("accessToken")
@@ -9,17 +13,25 @@ if (localStorage.getItem("accessToken") != null) {
 }
 
 // export const SelectOkay = async () => {
+//   console.log(user)
 //   const okay = await client.post(API_URL + "/manager/project/selectOkay");
 
 //   return okay;
 // };
 
 export const SelectOkay = async (index) => {
-  const okay = await client.post(API_URL + "/manager/project/selectOkay", {
+  if (user.roles == "ROLE_ADMIN") {
+     const okay = await client.post(API_URL + "/manager/project/selectOkay", {
     project_INDEX: index,
-  });
-
+     });
+    
+  console.log(okay)
   return okay;
+  }
+  else if (user.roles == "ROLE_TEAMLEADER") {
+    
+  }
+
 };
 
 export const UpdateOKay = async (index, okay) => {
